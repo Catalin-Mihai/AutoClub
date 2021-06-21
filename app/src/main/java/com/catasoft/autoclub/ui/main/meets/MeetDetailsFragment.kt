0498e8be1617370621d69670b91367ba9d9215d7
Catalin-Mihai <catalinnm99@gmail.com>
2021-06-21 23:32:56 +0300

    Add google maps support for place location details.

--- app/src/main/java/com/catasoft/autoclub/ui/main/meets/MeetDetailsFragment.kt
+++ app/src/main/java/com/catasoft/autoclub/ui/main/meets/MeetDetailsFragment.kt
@@ -1,5 +1,7 @@
 package com.catasoft.autoclub.ui.main.meets
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +10,7 @@ import android.view.ViewGroup
 import com.catasoft.autoclub.R
 import com.catasoft.autoclub.databinding.FragmentMeetDetailsBinding
 import com.catasoft.autoclub.model.meet.Meet
+import com.google.android.libraries.places.api.Places
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
@@ -28,5 +31,21 @@ class MeetDetailsFragment constructor(val meet: Meet): BottomSheetDialogFragment
 
         // Inflate the layout for this fragment
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnOpenMaps.setOnClickListener {
+
+            val gmmIntentUri =
+//                Uri.parse("https://www.google.com/maps/place/?q=place_id:${meet.placeId}")
+                Uri.parse("geo:0,0?q=${meet.placeLat},${meet.placeLong}(${meet.placeName})")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+//            mapIntent.setPackage("com.google.android.apps.maps")
+
+            mapIntent.resolveActivity(requireActivity().packageManager)?.let {
+                startActivity(mapIntent)
+            }
+        }
     }
 }
